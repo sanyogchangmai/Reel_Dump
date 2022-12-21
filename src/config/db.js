56b9117e41696dc8c46102ea75cf78beafd5234a
sanyogchangmai <sanyogchangmai29@gmail.com>
2022-12-21 14:41:02 +0530

    add env variable to db config

--- src/config/db.js
+++ src/config/db.js
@@ -2,26 +2,11 @@ import pg from "pg";
 // import logger from "../logger/logger.js";
 
 const pool = new pg.Pool({
-    user: "postgres",
-    password: "sanyog",
-    host: "localhost",
-    port: "5432",
-    database: "reel_dump_dev",
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
 });
-
-// const connectDB = async () => {
-//     try {
-//         const con = mysql.createConnection({
-//             host: "localhost",
-//             user: "root",
-//             password: "sanyog",
-//         });
-//         const connection = con.connect();
-//         logger.info(`Connected to DB ${connection}`);
-//     } catch (err) {
-//         logger.error(err);
-//         process.exit(1);
-//     }
-// };
 
 export default pool;
